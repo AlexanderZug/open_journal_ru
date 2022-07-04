5ae62d43b758a2b0c4c0d50040b91768851d8d8b
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-07-04 22:38:37 +0300

    add index view

--- journal_template/views.py
+++ journal_template/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import IndexPage
+
+
+def index(request):
+    index_list = IndexPage.objects.select_related().all()
+    context = {
+        'page_obj': index_list,
+    }
+    return render(request, 'index.html', context)
