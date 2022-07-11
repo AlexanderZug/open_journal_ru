743cb0f05e4b7ebfb98fad6e991da90e27319a82
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-07-11 17:26:13 +0300

    fix media root, add file downloading

--- open_journal_ru/urls.py
+++ open_journal_ru/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
 urlpatterns = [
@@ -24,4 +25,9 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+
+
