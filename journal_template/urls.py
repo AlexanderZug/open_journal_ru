a604377fc40efa40f8c0b9b1054bc52dcf8ac8c8
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-07-14 21:48:24 +0300

    add select_relate

--- journal_template/urls.py
+++ journal_template/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 
-from .views import (AboutView,
-                    ArchiveView,
-                    ArticleDetailView,
-                    IndexView,
+from .views import (AboutView, ArchiveView, ArticleDetailView, IndexView,
                     InformationView)
 
 app_name = 'journal_template'
