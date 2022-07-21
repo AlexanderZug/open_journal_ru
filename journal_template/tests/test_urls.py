3ba710ed11dcb5d78648af68b69e437df0c4850b
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-07-21 19:20:02 +0300

    add test url

---
+++ journal_template/tests/test_urls.py
@@ -0,0 +1,11 @@
+from django.test import TestCase, Client
+from http import HTTPStatus
+
+
+class StaticURLTests(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_homepage(self):
+        response = self.guest_client.get('/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
