072206fce2234afcb53015e544167d7313bcecb6
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-09-11 20:42:08 +0300

    add swagger

--- open_journal_ru/urls.py
+++ open_journal_ru/urls.py
@@ -1,29 +1,46 @@
-"""open_journal_ru URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import include, path
+from django.contrib import admin
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Open Journal API",
+        default_version='v1',
+        description="Documentation for Open Journal API",
+        contact=openapi.Contact(
+            email="alexandertsygankov.unterwegs@gmail.com"
+        ),
+        license=openapi.License(name="Apache License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', include('journal_template.urls', namespace='journals')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json',
+    ),
+    re_path(
+        r'^swagger/$',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui',
+    ),
+    re_path(
+        r'^redoc/$',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc',
+    ),
 ]
 
 handler404 = 'core.views.page_not_found'
